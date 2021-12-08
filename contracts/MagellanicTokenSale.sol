aec5f93b5d8322818d246ff06fc560b8810affd6
Mohd Numan <mohammadnuman123@gmail.com>
2021-12-08 18:28:00 +0530

    implented buy tokens partially

--- contracts/MagellanicTokenSale.sol
+++ contracts/MagellanicTokenSale.sol
@@ -3,17 +3,39 @@ pragma solidity >=0.4.20;
 import "./MagellanicToken.sol";
 
 contract MagellanictokenSale{
-    address public admin;
+    address admin;
     MagellanicToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokenSold;
+    
+    event Sell(
+        address indexed _buyer,
+        uint256 indexed _amount
+    );
 
     constructor(MagellanicToken _contract,uint256 _tokenPrice) public{
-        //set an admin
         admin=msg.sender;
         tokenContract=_contract;
         tokenPrice=_tokenPrice;
-        //Token contract
-        //set token price
+    }
 
+    //multiply function
+    function multiply(uint256 a, uint256 b) internal pure returns(uint c){
+        require(b==0 || (c=a*b)/b==a );
+    }
+
+    //buy tokens
+    function buyTokens(uint256 _numberOfTokens) public payable{
+        require(msg.value==multiply(_numberOfTokens,tokenPrice));
+
+        tokenSold+=_numberOfTokens;
+
+        emit Sell(msg.sender,_numberOfTokens);
+        //require that value is equal to tokens
+        //require if contract has enough tokens
+        //require that a transfer is successful
+   
+        
+        
     }
 }
--- test/MagellanicTokenSale.js
+++ test/MagellanicTokenSale.js
@@ -1,19 +1,52 @@
-const MagellanicTokenSale = artifacts.require("../contracts/MagellanicTokenSale.sol");
+const MagellanicTokenSale = artifacts.require(
+  "../contracts/MagellanicTokenSale.sol"
+);
 const assert = require("assert");
 
-
 contract("MagellanicTokenSale", function (accounts) {
+  var price = 1000000000000000;
+  var buyer = accounts[1];
+  var admin = accounts[0];
 
-    var price=1000000000000000;
+  it("initialises contract with the correct values", async () => {
+    const instance = await MagellanicTokenSale.deployed();
+    const address = await instance.tokenContract();
+    const tokenPrice = await instance.tokenPrice();
+    assert.notEqual(instance.address, "0x0");
+    assert.notEqual(address, "0x0");
+    assert.equal(tokenPrice, price);
+  });
 
-    it('initialises contract with the correct values',async()=>{
-        const instance = await MagellanicTokenSale.deployed();
-        const admin=await instance.admin();
-        const address=await instance.tokenContract();
-        const tokenPrice=await instance.tokenPrice();
-        assert.notEqual(instance.address,'0x0');
-        assert.notEqual(address,'0x0');
-        assert.equal(admin,accounts[0]);
-        assert.equal(tokenPrice,price);
+  it("facilitates token buying", async () => {
+    const instance = await MagellanicTokenSale.deployed();
+    var numberOfTokens = 10;
+    var value = numberOfTokens * price;
+    const receipt = await instance.buyTokens(numberOfTokens, {
+      from: buyer,
+      value: value,
     });
+    const amount = await instance.tokenSold();
+    assert.equal(amount.toNumber(), numberOfTokens);
+
+    assert.equal(receipt.logs.length, 1, "triggers one event");
+    assert.equal(receipt.logs[0].event, "Sell", 'should be the "Sell" event');
+    assert.equal(receipt.logs[0].args._buyer, accounts[1], "logs the buyer");
+    assert.equal(
+      receipt.logs[0].args._amount,
+      numberOfTokens,
+      "logs the numberOfTokens"
+    );
+
+    let done = false;
+    try {
+        const receipt = await instance.buyTokens(numberOfTokens, {
+            from: buyer,
+            value: 1,
+        });
+        done = true;
+    } catch (err) {
+      assert.equal(done, false);
+    }
+    assert.equal(done, false);
+  });
 });
